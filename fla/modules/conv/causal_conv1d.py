64eb0d3d6633489ce2711ea43ec9d81e66e9e7ea
huangjiyi <huangjiyi@users.noreply.github.com>
2026-07-30 12:33:05 +0800

    [KDA] Simplify ShortConvolution eager imports

--- fla/modules/conv/causal_conv1d.py
+++ fla/modules/conv/causal_conv1d.py
@@ -9,6 +9,8 @@
 
 import torch
 
+from fla.modules.conv.cp import causal_conv1d_cp
+from fla.modules.conv.triton import CausalConv1dFunction
 from fla.ops.cp import FLACPContext
 from fla.utils import input_guard
 
@@ -66,8 +68,6 @@ def causal_conv1d(
         If `output_final_state` is `False`, the final state is `None`.
     """
     if cp_context is not None:
-        from fla.modules.conv.cp import causal_conv1d_cp
-
         assert initial_state is None, "Initial state is not supported for CP"
         assert output_final_state is False, "Output final state is not supported for CP"
         output = causal_conv1d_cp(
@@ -81,8 +81,6 @@ def causal_conv1d(
         return output, None
 
     if backend == 'triton':
-        from fla.modules.conv.triton import CausalConv1dFunction
-
         y, final_state = CausalConv1dFunction.apply(
             x,
             weight,
